ee147e87494a0bd1a30654a12b9e1d925ce07569
Saurav Sen <sauravthakuri014@gmail.com>
2026-05-08 23:39:33 +0545

    day9

---
+++ Day_9/main.py
@@ -0,0 +1,20 @@
+
+# Dictionaries
+"""
+dictionary = {
+"Key" : "Value",
+"saurav" : "sen",
+"lionel" : "messi"}
+
+dictionary["saurav"]
+"""
+
+
+players = {
+    "lionel": "messi",
+    "cristiano": "ronaldo",
+    "luis": "suarez"
+}
+
+
+print(players["cristiano"])
