28adca9cd728e167e7799879b95e41eea4bede32
Saurav Sen <sauravthakuri014@gmail.com>
2026-05-09 23:59:04 +0545

    day_9 continue

--- Day_9/main.py
+++ Day_9/main.py
@@ -18,3 +18,62 @@ players = {
 
 
 print(players["cristiano"])
+
+
+
+
+"""
+Key        |               Value
+Bug        |     An error in  a program that prevents the program from running as expected.
+Function   |     A piece of code that you can easily call over and over again.
+Loop       |     The action of doing something over and over again
+
+{key: Value}
+
+"""
+
+programming_dictionary = {
+"Bug": 'An error in  a program that prevents the program from running as expected.',
+"Function": 'A piece of code that you can easily call over and over again.',
+"Loop": 'The action of doing something over and over again.',
+}
+
+# Retrieving a item from the dictionaries
+
+print(programming_dictionary["Bug"])  # Inside the square bracket you need to put key to get the value you want.
+
+print("\n")
+# print(programming_dictionary["Bog"]) # should throw KeyError
+
+# Add a piece of data
+
+programming_dictionary["Test"] = 'Adding one  item'
+
+print(programming_dictionary)
+
+print("\n")
+# Wiping an existing dictionary
+
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+
+# Edit an item in a dictionary
+
+print(programming_dictionary)
+
+programming_dictionary["Bug"] = "A moth in your computer"
+
+print("\n")
+print(programming_dictionary)
+
+
+print("\n")
+
+# Loop through a dictionary
+
+for thing in programming_dictionary:
+    print(thing)
+    print(programming_dictionary[thing])
+
+
--- Day_9/readme.md
+++ Day_9/readme.md
@@ -1,1 +1,8 @@
 # Day 9: Dictionaries, nesting, iteration.
+
+KeyError: 'Bog'
+Common Causes:
+1. Typographical Errors (Typos) --> instead of bug you put bog
+2. Use of ectra whitespace or hidden characters
+3. Key Missing in the dataset
+4. Case sensitivity Issues
